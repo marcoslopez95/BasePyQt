de38440442ab47e5dceca4fb33b33c75497d77ec
Marcos L <marcoslopez1895@gmail.com>
2023-08-03 10:36:18 -0400

    cambios

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import sys
-import random
 from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtWidgets import QApplication,QGroupBox, QHBoxLayout, QVBoxLayout, QWidget, QComboBox, QLineEdit, QPushButton, QSlider, QLabel
 from PySide6.QtCore import Qt
 
 class MyWidget(QtWidgets.QMainWindow):
+    volume = 50
+    speed  = 100
+    gender_voice = "Mujer"
     def __init__(self):
         super().__init__()
+
+        # set -> fijar, setear, asignar un valor 
+        # get -> recuperar, obtener
         self.setWindowTitle("Mi Aplicación")
         self.setGeometry(100, 100, 400, 200)
 
         # Crear los elementos de la interfaz
         self.gender_select = QComboBox()
-        self.gender_select.addItem("Hombre")
         self.gender_select.addItem("Mujer")
+        self.gender_select.addItem("Hombre")
         self.gender_select.setFixedWidth(100)  # Cambiar el ancho del QComboBox
         self.gender_select.currentIndexChanged.connect(self.on_gender_select_changed)
 
@@ -26,11 +31,13 @@ class MyWidget(QtWidgets.QMainWindow):
         self.play_button.clicked.connect(self.on_play_button_click)
 
         self.volume_slider1 = QSlider(Qt.Horizontal)
+        self.volume_slider1.setValue(self.volume)
         self.volume_slider1.setFixedWidth(150)  # Cambiar el ancho del QSlider
         self.volume_slider1.valueChanged.connect(self.on_slider2_value_changed)
 
         self.volume_slider2 = QSlider(Qt.Horizontal)
         self.volume_slider2.setFixedWidth(150)  # Cambiar el ancho del QSlider
+        self.volume_slider2.setValue(self.speed)
         self.volume_slider2.valueChanged.connect(self.on_slider2_value_changed)
         # Crear las etiquetas para las barras de volumen
         self.label1 = QLabel("Volumen 1")
@@ -55,6 +62,7 @@ class MyWidget(QtWidgets.QMainWindow):
         groupBox2.setLayout(layoutLabel2)
         groupBox2.setFixedHeight(70)  # Establecer tamaño fijo para groupBox
         groupBox2.setFixedWidth(170)  # Establecer tamaño fijo para groupBox
+
         top_layout.addWidget(groupBox) 
         top_layout.addWidget(groupBox2) 
 
@@ -72,24 +80,33 @@ class MyWidget(QtWidgets.QMainWindow):
         self.setCentralWidget(central_widget)
 
     def on_gender_select_changed(self, index):
-        gender = self.gender_select.currentText()
-        print("Selected gender:", gender)
+        self.gender_voice = self.gender_select.currentText()
+        print("Selected gender:", self.gender_voice)
 
     def on_play_button_click(self):
-        text_input_value = self.text_input.text()
-        print("Text input value:", text_input_value)
+        text_input_value = self.text_input.toPlainText()
+        # print("Text input value:", text_input_value)
+        cadena = f"""
+        volume: {self.volume}
+        speed:  {self.speed} 
+        voice_gender: {self.gender_voice}
+        texto: {text_input_value}
+        """
+        print(cadena)
 
     def on_slider1_value_changed(self, value):
+        self.volume = value
         print("Volume 1:", value)
 
     def on_slider2_value_changed(self, value):
+        self.speed = value
         print("Volume 2:", value)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
     widget = MyWidget()
-    widget.resize(800, 600)
+    # widget.resize(800, 600)
     widget.show()
 
     sys.exit(app.exec())
